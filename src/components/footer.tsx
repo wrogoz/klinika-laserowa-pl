104ee9629025f6ce9eb23dbb22541e06d91001e1
wrogoz <wrogoz@gmail.com>
2020-02-01 18:15:01 +0100

    e

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ export class Footer extends Component<{},{}> {
     render() {
         return (
             <footer>
-                footer
+                footerr
             </footer>
         )
     }
