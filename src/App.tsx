ef0d9a0f3ee484f0c23d194b8d8099e001aa7010
wrogoz <wrogoz@gmail.com>
2020-02-03 16:45:23 +0100

    issue4 grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import './style.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Layout from './layout';
 import { createGlobalStyle } from 'styled-components'
