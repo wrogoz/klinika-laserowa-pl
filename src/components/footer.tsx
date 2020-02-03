501d39c65c1cc4aef3330eeb26d88494ed3e6402
wrogoz <wrogoz@gmail.com>
2020-02-03 11:00:12 +0100

    footer in progress

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,29 +5,48 @@ import Col from 'react-bootstrap/Col';
 export class Footer extends Component<{},{}> {
     render() {
         return (
-            <FooterContainer>
-                <Row>
-                    <Col xs={12}>
-                    footerr
-                    </Col>
-                </Row>
 
-            </FooterContainer>
+                <FooterRow>
+                    <FooterCol xs={12}>
+                        <Row>
+                            <Col xs={12}>
+                                <p>2020 - Inter-Med</p>
+                            </Col>
+                        </Row>
+                        <Row noGutters={true}>
+                            <Col xs={6}>Obserwuj nas: I T Fb</Col>
+                            <Col xs={6}>
+                                <FooterStroke/>
+                            </Col>
+                    </Row>
+                    </FooterCol>
+                </FooterRow>
+
+
         )
     }
 }
 
 
-const FooterContainer = styled.footer`
+const FooterRow = styled(Row)`
 
     color:#fff;
     margin-top:auto;
+    padding: 15px 0 0 0;
     width:100%;
-    height:150px;
-    display:flex;
-    justify-content:center;
-    align-items:center;
     border-top:1px solid grey;
+    border-radius:15px 15px 0 0;
     background-color:#f7ad31;
+
+`
+const FooterCol = styled(Col)`
+    padding-bottom: 20px;
+    padding-right:0;
+`
+const FooterStroke = styled.div`
+    height:1px;
+    background-color:#fff;
+    width:100%;
+    margin-top:10px;
 `
 export default Footer
