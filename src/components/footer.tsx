ea7aec18e17dadd26988914fdd18ed2c56ea5536
wrogoz <wrogoz@gmail.com>
2020-02-07 08:19:12 +0100

    added social icons to main page

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import styled from 'styled-components';
+import instagram from '../images/instagram.png';
+import twitter from '../images/twitterLogo.png';
+import facebook from '../images/facebook.png';
 
 export class Footer extends Component<{},{}> {
     render() {
@@ -9,7 +12,10 @@ export class Footer extends Component<{},{}> {
 
                             <p>2020 - Inter-Med</p>
                             <SecondRow>
-                            <p>Obserwuj nas: I T Fb</p>
+                            <p>Obserwuj nas:</p>
+                            <Social src={instagram} alt="instragram"/>
+                            <Social src={twitter} alt="twitter"/>
+                            <Social src={facebook} alt="facebook"/>
                             <FooterStroke/>
                             </SecondRow>
 
@@ -38,11 +44,17 @@ const FooterBox = styled.footer`
 const SecondRow = styled.div`
     display:flex;
     justify-content:space-between;
+    align-items:center;
 `
 const FooterStroke = styled.div`
     height:1px;
     background-color:#fff;
-    width:48%;
+    width:44%;
     align-self:center;
+`
+const Social = styled.img`
+height: 15px;
+width: 15px;
+margin: 0 5px;
 `
 export default Footer
